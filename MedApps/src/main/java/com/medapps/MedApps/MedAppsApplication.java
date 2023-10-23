a89dc3b4a40341d83a8b1fd739b00dff8bed1b88
harshsinhaprince <136982625+harshsinhaprince@users.noreply.github.com>
2023-10-23 14:01:22 +0530

    new project creation

---
+++ MedApps/src/main/java/com/medapps/MedApps/MedAppsApplication.java
@@ -0,0 +1,13 @@
+package com.medapps.MedApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedAppsApplication.class, args);
+	}
+
+}
---
+++ MedApps/src/test/java/com/medapps/MedApps/MedAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medapps.MedApps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
