758ae51602ef8ced20ac4af99ce7696b7b658ab4
harshsinhaprince <136982625+harshsinhaprince@users.noreply.github.com>
2023-11-20 18:07:49 +0530

    Validation added to User entity

--- UserService/src/main/java/com/medapps/UserService/entity/User.java
+++ UserService/src/main/java/com/medapps/UserService/entity/User.java
@@ -7,21 +7,34 @@ import javax.persistence.Id;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 @Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String username;
-    private String password;
-    private String email;
-    private String phone;
-    private String otp;
-    private boolean isActive; // To track if the account is activated
-    
-    
-    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "username is required")
+	private String username;
+	
+	@NotBlank(message = "password is required")
+	@Size(min = 8, message = "password must be of 8 character")
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=]).*$", message = "Password must contain at least one digit, one letter, and one special character")
+	private String password;
+	
+	@Email(message = "Invalid email format")
+	private String email;
+	
+	private String phone;
+	
+	private String otp;
+	
+	private boolean isActive; // To track if the account is activated
+
 	public User(Long id, String username, String password, String email, String phone, String otp, boolean isActive) {
 		super();
 		this.id = id;
@@ -32,50 +45,62 @@ public class User {
 		this.otp = otp;
 		this.isActive = isActive;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
-		BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		this.password = bCryptPasswordEncoder.encode(password);
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getOtp() {
 		return otp;
 	}
+
 	public void setOtp(String otp) {
 		this.otp = otp;
 	}
+
 	public boolean isActive() {
 		return isActive;
 	}
+
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
 	}
-    
-    
-}
 
+}
